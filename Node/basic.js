476748a177404c813c8ee42a6aa298b7d6d0b767
Dev-SK01 <srik3713@gmail.com>
2024-03-17 15:18:09 +0530

    Exception handling

--- Node/basic.js
+++ Node/basic.js
@@ -2,16 +2,16 @@ const os = require('os') ;
 const path = require('path');
 const math = require('./export')
 
-// // node JS global oject
-// console.log(global);
+// node JS global oject
+console.log(global);
 
-// // Node JS Core Module
-// console.log(os.type() ,os.version() ,os.homedir() ,os.networkInterfaces());
-// // path module
-// console.log(path);
-// console.log(path.parse(__filename));
-// // deafult values Node provide
-// console.log(__dirname);
-// console.log(__filename)
+// Node JS Core Module
+console.log(os.type() ,os.version() ,os.homedir() ,os.networkInterfaces());
+// path module
+console.log(path);
+console.log(path.parse(__filename));
+// deafult values Node provide
+console.log(__dirname);
+console.log(__filename)
 // using imported exported functions
 console.log(math.add(1,2), math.sub(12,100) , math.mul(9,2) , math.div(14,7))
--- Node/fileSystem.js
+++ Node/fileSystem.js
@@ -2,10 +2,16 @@ const fs = require('node:fs');
 const path = require('path');
 
 
-fs.readFile(path.join(__dirname ,'../textFiles' ,'nodeJS.txt') , (err , data) =>{
+fs.readFile(path.join(__dirname ,'../textFiles' ,'nodeJSs.txt') , (err , data) =>{
     if(err) throw err
-    console.log('\tBuffer Data : \n\n' , data)
-    console.log('\tActual Data : \n\n' ,data.toString())
+    // JS methos to try catch 
+    try{
+        console.log('\tBuffer Data : \n\n' , data)
+        console.log('\tActual Data : \n\n' ,data.toString())
+    }catch(err){
+        console.err(err)
+    }
+    
 });
 console.log(__dirname)
 // Handling the errors 
